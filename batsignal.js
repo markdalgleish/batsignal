cf1783254c2057610abfeeb86c147d8e44d162df
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-08-28 12:13:02 +1000

    Initial commit

---
+++ batsignal.js
@@ -0,0 +1,51 @@
+var Messages = new Meteor.Collection('messages');
+
+Messages.add = function(message) {
+  if (typeof message === 'string') {
+    this.insert({message: message, isRead: false});
+    return true;
+  }
+  return false;
+};
+
+if (Meteor.is_server) {
+  Meteor.publish('messages', function() {
+    return Messages.find({}, {sort: {$natural: -1}, limit: 1});
+  });
+}
+
+if (Meteor.is_client) {
+  Meteor.subscribe('messages');
+
+  var submit = function() {
+    var textbox = document.getElementById('text');
+    if (Messages.add(textbox.value)) {
+      textbox.value = '';
+    }
+  };
+  
+  Template.main.messages = function() {
+    return Messages.find({});
+  };
+
+  Template.main.events = {
+    'click button': function () {
+      submit();
+    },
+    'keypress input': function(e) {
+      if (e.which === 13) {
+        submit();
+      }
+    }
+  };
+
+  Template.message.className = function() {
+    return this.isRead ? 'read' : 'unread';
+  };
+
+  Template.message.events = {
+    'click' : function() {
+      Messages.update(this, {$set: {isRead: true}});
+    }
+  };
+}
