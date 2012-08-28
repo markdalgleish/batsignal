3b33503c395419c1507e67176ee68b070d521aaf
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-08-28 14:26:19 +1000

    Clean up code

--- batsignal.js
+++ batsignal.js
@@ -1,55 +1,53 @@
 var Messages = new Meteor.Collection('messages');
 
 Messages.add = function(message) {
-  if (typeof message === 'string') {
-    this.insert({message: message, isRead: false});
-    return true;
-  }
-  return false;
+	if (typeof message === 'string') {
+		this.insert({message: message, isRead: false});
+		return true;
+	}
+	return false;
 };
 
 if (Meteor.is_server) {
-  Meteor.publish('messages', function() {
-    return Messages.find({}, {sort: {$natural: -1}, limit: 1});
-  });
+	Meteor.publish('messages', function() {
+		return Messages.find({}, {sort: {$natural: -1}, limit: 1});
+	});
 }
 
 if (Meteor.is_client) {
-  Meteor.subscribe('messages');
+	Meteor.subscribe('messages');
 
-  var submit = function() {
-    var textbox = document.getElementById('text');
-    if (Messages.add(textbox.value)) {
-      textbox.value = '';
-    }
-  };
-  
-  Template.main.messages = function() {
-    return Messages.find({});
-  };
+	var submit = function() {
+		var textbox = document.getElementById('text');
+		if (Messages.add(textbox.value)) textbox.value = '';
+	};
 
-  Template.main.inputClassName = function() {
-    return Messages.findOne({}) && Messages.findOne({}).isRead ? 'read' : 'unread';
-  };
+	var getMessage = function() {
+		return Messages.findOne({});
+	}
+	
+	Template.main.messages = function() {
+		return Messages.find({});
+	};
 
-  Template.main.events = {
-    'click button': function () {
-      submit();
-    },
-    'keypress input': function(e) {
-      if (e.which === 13) {
-        submit();
-      }
-    }
-  };
+	Template.main.inputClassName = function() {
+		return getMessage() && getMessage().isRead ? 'read' : 'unread';
+	};
 
-  Template.message.className = function() {
-    return this.isRead ? 'read' : 'unread';
-  };
+	Template.main.events = {
+		'click button': submit,
+		'keypress input': function(e) {
+			e.which === 13 && submit();
+		}
+	};
 
-  Template.message.events = {
-    'click' : function() {
-      Messages.update(this, {$set: {isRead: true}});
-    }
-  };
+	Template.message.className = function() {
+		return this.isRead ? 'read' : 'unread';
+	};
+
+	Template.message.events = {
+		'click' : function() {
+			Messages.update(this, {$set: {isRead: true}});
+		}
+	};
 }
